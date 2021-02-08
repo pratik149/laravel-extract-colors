a4c7310e221d06d49a4a2dc2cc50efc0e12534c7
Pratik Rane <pratikrane149@gmail.com>
2021-02-08 06:47:40 +0530

    complete model and migration for Image

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+	protected $fillable = [
+        'name',
+        'image_path'
+    ];
 }
--- database/migrations/2021_02_08_011355_create_images_table.php
+++ database/migrations/2021_02_08_011355_create_images_table.php
@@ -15,7 +15,11 @@ class CreateImagesTable extends Migration
     {
         Schema::create('images', function (Blueprint $table) {
             $table->id();
+			$table->string('name');
+	        $table->string('path');
             $table->timestamps();
+
+			$table->engine = 'InnoDB';
         });
     }
 
