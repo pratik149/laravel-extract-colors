24f0fb1cf856055fb94f9e2896f5445066399275
Pratik Rane <pratikrane149@gmail.com>
2021-02-08 12:53:31 +0530

    create routes for uploading and extracting color from images

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ImageUploadController;
+use App\Http\Controllers\ImageExtractColorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('image-upload', [ ImageUploadController::class, 'index' ])->name('image.index');
+Route::post('image-upload', [ ImageUploadController::class, 'store' ])->name('image.save');
+Route::get('extract-colors/{id}', [ ImageExtractColorController::class, 'extractColors' ])->name('image.extract-colors');
