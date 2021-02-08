26a179e404b1c691141df5e95ed03e36034b84b6
Pratik Rane <pratikrane149@gmail.com>
2021-02-08 13:02:58 +0530

    add link in home page to upload image page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,6 +35,10 @@
                     @endauth
                 </div>
             @endif
+			<div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+				<a href="{{ route('image.index') }}" class="ml-4 text-sm text-gray-700 underline">Image Upload</a>
+			</div>
+
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
